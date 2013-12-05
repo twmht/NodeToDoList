bc7e1c99ea3a420cbb3e793319c84c8ef53555f1
mht <qrnnis2623891@gmail.com>
2013-12-05 15:47:54 +0800

    add sort

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,13 @@
  */
 var mongoose = require('mongoose');
 var Todo = mongoose.model('Todo');
+var title = "NodeTodoList"
+var utils = require('connect').utils;
 
 exports.index = function(req, res){
-  //res.render('index', { title: 'Express' });
-  Todo.find(function(err,todos,count){
+  Todo.find().sort('-updated_at').exec(function(err,todos){
       res.render('index',{
-          title: 'Express Todo',
+          title: title,
           todos: todos
       });
   })
@@ -36,7 +37,7 @@ exports.destroy = function(req,res){
 exports.edit = function(req,res){
     Todo.find(function(err,todos){
         res.render('edit',{
-            title: 'Express Todo',
+            title: title,
             todos: todos,
             current : req.params.id
         });
@@ -51,4 +52,12 @@ exports.update = function(req,res){
             res.redirect('/');
         });
     });
+};
+
+exports.current_user = function(req,res,next){
+    var user_id = req.cookies? req.cookies.user_id : undefined;
+    if(!user_id){
+        res.cookie('user_id',utils.uid(32));
+    }
+    next();
 };
